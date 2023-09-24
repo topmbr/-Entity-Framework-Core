362131ce57e0bfa21cff723fe6a46578ffc1a6c3
topmbr <centerm9911@gmail.com>
2023-09-24 16:31:54 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp47/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace ConsoleApp47
+{
+    class UserContext : DbContext
+    {
+        public UserContext() 
+            :base("USERS")
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
